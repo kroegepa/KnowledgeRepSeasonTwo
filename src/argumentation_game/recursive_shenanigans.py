558254f5e2bf5d069015767ec304698e62dd6e88
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-12-11 16:56:35 +0100

    Fix sub-par's programmer's mistake

--- src/argumentation_game/recursive_shenanigans.py
+++ src/argumentation_game/recursive_shenanigans.py
@@ -1,8 +1,12 @@
 from argumentation_game.argument_graph import ArgumentGraph
-from typing import List,Optional
-#Labeling is list of integers
-#1 is in 2 is out 0 undecided
-def recursive_boogaloo(argument_graph: ArgumentGraph, labeling: List[int], current_node: int)-> Optional[list] :
+from typing import List, Optional
+
+
+# Labeling is list of integers
+# 1 is in 2 is out 0 undecided
+def recursive_boogaloo(
+    argument_graph: ArgumentGraph, labeling: List[int], current_node: int
+) -> Optional[List[int]]:
     if labeling[current_node] == 1:
         changed_flag = 0
         for attacker in argument_graph.arguments[current_node].attackers:
@@ -14,8 +18,12 @@ def recursive_boogaloo(argument_graph: ArgumentGraph, labeling: List[int], curre
         if changed_flag == 0:
             return labeling
         for attacker in argument_graph.arguments[current_node].attackers:
-            recursive_boogaloo(argument_graph,labeling,attacker)
-#If current_node is out
+            labeling = recursive_boogaloo(argument_graph, labeling, attacker)
+            if labeling is None:
+                return None
+        return labeling
+
+    # If current_node is out
     elif labeling[current_node] == 2:
         for attacker in argument_graph.arguments[current_node].attackers:
             temp_labeling = labeling
@@ -23,10 +31,12 @@ def recursive_boogaloo(argument_graph: ArgumentGraph, labeling: List[int], curre
                 temp_labeling = None
             elif labeling[attacker] == 0:
                 temp_labeling[attacker] = 1
-                temp_labeling = recursive_boogaloo(argument_graph,temp_labeling, attacker)
-            elif labeling[attacker] == 1 :
+                temp_labeling = recursive_boogaloo(
+                    argument_graph, temp_labeling, attacker
+                )
+            elif labeling[attacker] == 1:
                 return labeling
-            
+
             if temp_labeling is not None:
                 return temp_labeling
     return None
